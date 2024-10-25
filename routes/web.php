e602ff92e7a8dddc78c7f573689db92813907186
Duvingrand <Duvingrand@gmail.com>
2024-10-25 08:57:05 -0500

    feat: Creation of Routes of Events

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,11 @@ Route::resource('users', UserController::class);
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('Events', [EventController::class, 'index'])->name('Events.index');
+Route::get('Events/create', [EventController::class, 'create'])->name('Events.create');
+Route::post('Events', [EventController::class, 'store'])->name('Events.store');
+Route::get('Events/{id}', [EventController::class, 'show'])->name('Events.show');
+Route::get('Events/{id}/edit', [EventController::class, 'edit'])->name('Events.edit');
+Route::put('Events/{id}', [EventController::class, 'update'])->name('Events.update');
+Route::delete('Events/{id}', [EventController::class, 'destroy'])->name('Events.destroy');
