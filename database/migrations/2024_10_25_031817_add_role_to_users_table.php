0a4d352e4a7351b24aca309f19e0b829b770a0e5
CODEALEJO <salomee579@gmail.comgit config --global user.email salomee579@gmail.comgit config --global user.email salomee579@gmail.com>
2024-10-24 22:31:46 -0500

    feat: the user model was created

--- database/migrations/2024_10_25_031817_add_role_to_users_table.php
+++ database/migrations/2024_10_25_031817_add_role_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('role')->default('user'); // Agrega el campo role a la tabla users
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('role'); // Elimina el campo role si se hace un rollback
         });
     }
 };
