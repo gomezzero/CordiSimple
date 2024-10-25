9ac8ef16704d3372d5086ff77c937e2743810629
EdgarC97 <ecardona742@gmail.com>
2024-10-25 12:12:38 -0500

    feat: Add route configurations

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,13 @@ use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\EventController;
 Route::resource('users', UserController::class);
 Route::resource('notifications', NotificationController::class);
+
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+
 // Incluir las rutas de autenticación
-require __DIR__.'/auth.php';
 Route::get('Events', [EventController::class, 'index'])->name('Events.index');
 Route::get('Events/create', [EventController::class, 'create'])->name('Events.create');
 Route::post('Events', [EventController::class, 'store'])->name('Events.store');
@@ -18,3 +20,5 @@ Route::get('Events/{id}', [EventController::class, 'show'])->name('Events.show')
 Route::get('Events/{id}/edit', [EventController::class, 'edit'])->name('Events.edit');
 Route::put('Events/{id}', [EventController::class, 'update'])->name('Events.update');
 Route::delete('Events/{id}', [EventController::class, 'destroy'])->name('Events.destroy');
+
+require __DIR__.'/auth.php';
