8df79de161fb49bdbe3a4a037eb5b6725ff54b34
Duvingrand <Duvingrand@gmail.com>
2024-10-25 11:10:52 -0500

    fix: rename of the branch

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Event;
-use Illuminate\Http\Requests\EventRequest;
+use App\Http\Requests\EventRequest;
 
 class EventController extends Controller
 {
