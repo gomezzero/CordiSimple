cfe7064ac17576907a1523a424bad7d6fd70eaa6
EdgarC97 <ecardona742@gmail.com>
2024-10-25 12:27:24 -0500

    feat: Add login routes

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,19 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\EventController;
+
 Route::resource('users', UserController::class);
 Route::resource('notifications', NotificationController::class);
+
+
 Route::get('/', function () {
     return view('welcome');
 });
-// Incluir las rutas de autenticación
-require __DIR__.'/auth.php';
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::get('events', [EventController::class, 'index'])->name('events.index'); // Cambiado a 'events.index'
 Route::get('events/create', [EventController::class, 'create'])->name('events.create'); // Cambiado a 'events.create'
@@ -19,3 +25,6 @@ Route::get('events/{id}', [EventController::class, 'show'])->name('events.show')
 Route::get('events/{id}/edit', [EventController::class, 'edit'])->name('events.edit'); // Cambiado a 'events.edit'
 Route::put('events/{id}', [EventController::class, 'update'])->name('events.update'); // Cambiado a 'events.update'
 Route::delete('events/{id}', [EventController::class, 'destroy'])->name('events.destroy'); // Cambiado a 'events.destroy'
+
+// Incluir las rutas de autenticación
+require __DIR__.'/auth.php';
