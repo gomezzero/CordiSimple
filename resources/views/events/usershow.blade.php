edb18ee2bc33fe39ecc7c9404977423ff5df4a65
Duvingrand <Duvingrand@gmail.com>
2024-10-28 13:00:03 -0500

    fix: correction of usershow event

--- resources/views/events/usershow.blade.php
+++ resources/views/events/usershow.blade.php
@@ -2,6 +2,7 @@
 
 @section('title', 'Detalles del Evento')
 
+@section('content')
     <div class="container mx-auto py-8 px-4">
         <div class="bg-white shadow-md rounded-lg p-6">
             <h1 class="text-3xl font-bold text-center mb-6">{{ $event->name }}</h1>
