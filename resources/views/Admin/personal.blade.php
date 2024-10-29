e863f40d63524b0e2f27cb4e07386211cef63d9a
gomezzero <gomezjuanzero@gmail.com>
2024-10-29 08:10:56 -0500

    fix: fixing booking view errors and adding logic in booking controller

--- resources/views/Admin/personal.blade.php
+++ resources/views/Admin/personal.blade.php
@@ -20,17 +20,18 @@
         </div>
     </main>
 
-    @if (session('error'))
+    @if (session('success'))
+        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" role="alert">
+            <strong class="font-bold">¡Éxito!</strong>
+            <span class="block sm:inline">{{ session('success') }}</span>
+        </div>
+    @else
+    if (session('error'))
     <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
         <strong class="font-bold">No Puedes</strong>
         <span class="block sm:inline">{{ session('error') }}</span>
     </div>
 
-    @else (session('success'))
-        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" role="alert">
-            <strong class="font-bold">¡Éxito!</strong>
-            <span class="block sm:inline">{{ session('success') }}</span>
-        </div>
 @endif
 
 </html>
